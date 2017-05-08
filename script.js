5e61c94e0cb3cc7533bc538d5153dfda047589f4
sota1235 <sota1235@gmail.com>
2017-05-08 20:52:10 +0900

    modify: add some sample button

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const database = firebase.database();
 
 const getAllDataButtonElement = document.getElementById('get-all-data');
+const postDataButtonElement = document.getElementById('post-data');
+const postDataValueElement = document.getElementById('post-data-value');
 
 function writeUserDate(userId, name, email, imageUrl) {
   database.ref(`users/${userId}`).set({
@@ -10,12 +12,19 @@ function writeUserDate(userId, name, email, imageUrl) {
   });
 };
 
-database.ref('users/').on('value', snapshot => {
+database.ref().on('value', snapshot => {
   console.log(snapshot.val());
 });
 
 getAllDataButtonElement.addEventListener('click', () => {
   database.ref().once('value').then(snapshot => {
     console.log(snapshot.val());
+  });
+});
+
+postDataButtonElement.addEventListener('click', () => {
+  const value = postDataValueElement.value;
+  database.ref(`data/${value}`).set({
+    value: value,
   });
 });
