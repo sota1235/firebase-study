d5483fdf196ec5c3dc609b218bdaa1943d27f30a
sota1235 <sota1235@gmail.com>
2017-05-08 20:30:41 +0900

    add: script for auth

---
+++ auth.sample.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+const config = {
+  apiKey: "API KEY",
+  authDomain: "AUTH DOMAIN",
+  databaseURL: "DATABASE URL",
+  projectId: "PROJECT ID",
+  storageBucket: "STORAGE BUCKET",
+  messagingSenderId: "MESSAGING SENDER ID"
+};
+firebase.initializeApp(config);
+
